80b5498d121f7e892f8bee647fe4cf2f8acdc901
ramdafe <dafe.ram@gmail.com>
2024-06-05 14:11:40 +0530

    Commit

--- Selenium With Python/Hello World.py
+++ Selenium With Python/Hello World.py
@@ -1,2 +1,7 @@
 #Hello World Program
-print("Hello World")
+print("Hello World", "Of Universe", 2)
+is_active = True
+if is_active == False:
+    print("The user is inactive.")
+else:
+    print("The user is active")
