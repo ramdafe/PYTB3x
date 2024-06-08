c07746e33d15e10e8d76695bb94d3a403c5e5040
ramdafe <dafe.ram@gmail.com>
2024-06-08 10:27:17 -0400

    Adding new files

---
+++ Selenium With Python/PYT3xAT/Ex_05062024/Lab002.py
@@ -0,0 +1,8 @@
+#Code to create a pyramid
+rows = int(input("How many rows do you want?: "))
+for i in range(0,rows):
+    for j in range(rows-1, i, -1):
+        print(" ", end="")
+    for j in range(0,i+1):
+        print("* ", end="")
+    print()
