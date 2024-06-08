9cb5439b0d0395477c819bd8d64f6591837b41ff
ramdafe <dafe.ram@gmail.com>
2024-06-08 11:07:04 -0400

    Code update to Pyhton Program

--- Selenium With Python/PYT3xAT/Ex_07062024/Lab004.py
+++ Selenium With Python/PYT3xAT/Ex_07062024/Lab004.py
@@ -4,3 +4,9 @@ age = input("How old are you? : ")
 print("Your name is", name, end="_", sep=" ")
 print("Your age is", age, end="\n", sep=":")
 print("Your double age", str(int(age) * 2), sep=" = ")
+
+dir1 = 'C:\nomedir\some dir'
+
+# Raw String
+dir2 = r'C:\nomedir\some dir'
+print(dir)
