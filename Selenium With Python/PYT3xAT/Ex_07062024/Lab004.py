c0c26564852dbd03048c5e89246dd74e675ab873
ramdafe <dafe.ram@gmail.com>
2024-06-08 09:00:24 -0400

    Adding new lines of code related to LIST.

--- Selenium With Python/PYT3xAT/Ex_07062024/Lab004.py
+++ Selenium With Python/PYT3xAT/Ex_07062024/Lab004.py
@@ -7,7 +7,23 @@ print(len(shopping_list))
 
 shopping_list.append("Chocolate")
 shopping_list.extend(["Chips", "Sugar", "Oats"])
-shopping_list.insert(1, "Butter")
+shopping_list.insert(0, "Butter")
+
+# x not in list
+# shopping_list.remove("CURD")
+
+shopping_list.remove("Chips")
 
 print(shopping_list)
+
+shopping_list.pop(1)
+print(shopping_list)
 print(len(shopping_list))
+
+# Sort the List
+shopping_list.sort()
+print(shopping_list)
+
+#Reverse the List
+shopping_list.reverse()
+print(shopping_list)
