e57b7ad8d72c8257872f3a893de35811aa7ae58b
ramdafe <dafe.ram@gmail.com>
2024-06-12 07:28:07 -0400

    Commiting a code change at line #10

--- Selenium With Python/PYT3xAT/Ex_12062024/Assignment/Lab003_Factorial.py
+++ Selenium With Python/PYT3xAT/Ex_12062024/Assignment/Lab003_Factorial.py
@@ -7,6 +7,6 @@
 
 num = int(input("Enter the number: "))
 factorial_val = 1
-for i in range(num, 1, -1):
+for i in range(num, 0, -1):
     factorial_val *= i
 print('The factorial value of ',str(num), ' is ', str(factorial_val))
